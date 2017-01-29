051b4c1e345115fe8bb83fe8adaf42bc75ca8b8f
Marcelo Gornstein <marcelog@gmail.com>
2017-01-28 21:09:19 -0300

    prepared for release

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Servito.Mixfile do
     [app: :servito,
      version: "0.0.10",
      elixir: "> 1.0.0",
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -12,9 +14,28 @@ defmodule Servito.Mixfile do
     [applications: [:logger, :cowboy]]
   end
 
+  defp description do
+    """
+Launches HTTP servers for testing
+
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Marcelo Gornstein"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/marcelog/servito"
+      }
+    ]
+  end
+
   defp deps do
     [
       {:cowboy, "~> 1.0.4"},
+      {:ex_doc, "~> 0.14.5", only: :dev},
       {:exjsx, "~> 3.2.1"},
       {:sweet_xml, "~> 0.6.4"},
       {:ibrowse, "~> 4.4.0", only: :test},
